7a038e972ca26302d1666422bae95ccaa4af96c3
Dachi Darjania <dachidar19@gmail.com>
2025-10-15 17:27:41 +0400

    davamate

--- script.js
+++ script.js
@@ -2,12 +2,16 @@
 
 function showCartContainer(){
   let cartContainer = document.querySelector(".header__cart-container")
+  let darkBg = document.querySelector(".header__dark-bg")
   cartContainer.classList.add("header__cart-container--open")
+  darkBg.style.display = "unset"
 }
 
 function closeCartContainer(){
   let cartContainer = document.querySelector(".header__cart-container")
+  let darkBg = document.querySelector(".header__dark-bg")  
   cartContainer.classList.remove("header__cart-container--open")
+  darkBg.style.display = "none"
 }
 
 
