5d04cf009e5dd955de9fd33ea87d04e599042330
Dachi Darjania <dachidar19@gmail.com>
2025-10-17 19:57:43 +0400

    davamate kategoriebis funqcia

--- other pages/order-online/order.js
+++ other pages/order-online/order.js
@@ -43,14 +43,6 @@ function hideMobileFilter(){
 
 
 
-
-
-let mobileSelect = document.getElementById("mobileSelect")
-fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
-.then( res => res.json())
-.then(data => data.forEach(item => mobileSelect.innerHTML += `<option value="${item.id}">${item.name}</option>`))
-
-
 let categories = document.querySelector(".menu__categories")
 fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
 .then(res => res.json())
@@ -58,5 +50,56 @@ fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
 //needs a function
 
 
+function setCategory(id){
+  menuContainer.innerHTML = ""
+  fetch(`https://restaurant.stepprojects.ge/api/Categories/GetCategory/${id}`)
+  .then(res => res.json())
+  .then(data => data.products.forEach(item => menuContainer.innerHTML += card(item)))
+}
 
 
+
+let mobileSelect = document.getElementById("mobileSelect")
+fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
+.then( res => res.json())
+.then(data => data.forEach(item => mobileSelect.innerHTML += `<option value="${item.id}">${item.name}</option>`))
+
+let desktopSelect = document.getElementById("desktopSelect")
+fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
+.then(res => res.json())
+.then(data => data.forEach(item => desktopSelect.innerHTML += `<option value="${item.id}">${item.name}</option>`))
+
+
+
+
+
+
+
+
+
+let menuContainer = document.querySelector(".menu__container")
+function showAll(){
+menuContainer.innerHTML = ""
+fetch("https://restaurant.stepprojects.ge/api/Products/GetAll")
+.then(res => res.json())
+.then(data => data.forEach(product => menuContainer.innerHTML += card(product)))
+}
+
+showAll()
+
+
+function card(product){
+  return `<div class="menu__product-card">
+            <img class="menu__product-image" src="${product.image}" alt="${product.name}">
+            <div class="menu__product-lower-side">
+              <h2 class="menu__product-name">${product.name}</h2>
+              <div class="menu__vegnut-container">
+                <p class="menu__product-vegeterian">Vegeterian <i style="color: ${product.vegeterian ? 'green' : 'darkred'}" class="fa-solid fa-${product.vegeterian ? 'check' : 'x'}"></i></p>
+                <p class="menu__product-nuts">Nuts <i style="color: ${product.nuts ? 'green' : 'darkred'}" class="fa-solid fa-${product.nuts ? 'check' : 'x'}"></i></p>
+              </div>
+              <p class="menu__product-spiciness">Spiciness Level: ${product.spiciness}</p>
+              <p class="menu__product-price">$${product.price}</p>
+            </div>
+            <button class="menu__cart-btn">ADD TO CART</button>
+          </div>`
+}
