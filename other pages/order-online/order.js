96b32c9a467d51a4adc23b2c51573368a0606999
Dachi Darjania <dachidar19@gmail.com>
2025-10-17 18:17:06 +0400

    davamate mobile filter ufuncqcio

--- other pages/order-online/order.js
+++ other pages/order-online/order.js
@@ -22,3 +22,41 @@ function closeMenu(){
   let burgerMenu = document.querySelector(".header__menu")
   burgerMenu.classList.remove("header__menu--open")
 }
+
+
+
+
+
+function openMobileFilter(){
+  let filterMobile = document.querySelector(".mobile-filter")
+  let darkBg = document.querySelector(".menu__dark-bg")
+  filterMobile.classList.add("mobile-filter--active")
+  darkBg.classList.add("menu__dark-bg--active")
+}
+
+function hideMobileFilter(){
+  let filterMobile = document.querySelector(".mobile-filter")
+  let darkBg = document.querySelector(".menu__dark-bg")
+  filterMobile.classList.remove("mobile-filter--active")
+  darkBg.classList.remove("menu__dark-bg--active")
+}
+
+
+
+
+
+let mobileSelect = document.getElementById("mobileSelect")
+fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
+.then( res => res.json())
+.then(data => data.forEach(item => mobileSelect.innerHTML += `<option value="${item.id}">${item.name}</option>`))
+
+
+let categories = document.querySelector(".menu__categories")
+fetch("https://restaurant.stepprojects.ge/api/Categories/GetAll")
+.then(res => res.json())
+.then(data => data.forEach(category => categories.innerHTML += `<li onclick="setCategory(${category.id})" class="menu__category">${category.name.toUpperCase()}</li>`))
+//needs a function
+
+
+
+
